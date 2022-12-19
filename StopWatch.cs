cbc1e2c3082eb5ef46901e6d6affe4891a7eec01
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-19 14:00:57 +0530

    Stopwatch Simulate

---
+++ StopWatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    public class StopWatch
+    {
+        public static void Simulate()
+        {
+            // Create new stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+            // Begin timing
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("HI");
+            }
+            // Stop timing
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}",
+         stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+}
