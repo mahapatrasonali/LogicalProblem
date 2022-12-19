8e222f7b346718476f9c968264160cb66970f8da
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-19 08:26:54 +0530

    Logical Problems

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //FibonacciSeries.DisplayFibonacciSeries();
+            PerfectNumber.FindNumbers();
+        }
+    }
+}
