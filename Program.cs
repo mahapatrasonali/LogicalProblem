9eb40668138e09d413f865c8acb58cffc4a032cd
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-19 08:51:26 +0530

    Reverse Number

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@
         public static void Main(string[] args)
         {
             //FibonacciSeries.DisplayFibonacciSeries();
-            PerfectNumber.FindNumbers();
+            // PerfectNumber.FindNumbers();
+            ReverseNumber.Display();
         }
     }
 }
